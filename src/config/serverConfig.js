c9372928b9d583b1fb9e65269782d83df22ffa43
vivekChenna <chennavivek96@gmail.com>
2022-12-13 18:17:37 -0700

    basic setup of bodyParser

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,11 +1,16 @@
 const dotenv = require('dotenv');
 
+// const path = require('path');
+
+// require('dotenv').config({path : path.resolve(__dirname,'.../.env')});
+
+// console.log(path);
+
 dotenv.config();
 
 
 module.exports = {
 
-
-    PORT : process.env.P
+    PORT : process.env.PORT
 
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 const express = require("express");
 
-const {PORT} = require('./config/serverConfig');
+const  {PORT} = require('./config/serverConfig');
 
 const BodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 const setUpAndStartServer = async () => {
   const app = express();
 
+  // const PORT  = 3000;
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended:true})); 
 
   console.log(PORT);
+
   app.listen(PORT, () => {
     console.log(`server started successfully at ${PORT} `);
   });
 };
-
 
 setUpAndStartServer();
